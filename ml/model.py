58a972fce9b630e3ab82653b3dcc071fa99abce9
John Crosby <jcrosby67@gmail.com>
2024-12-14 20:45:03 -0600

    Step 3 Complete

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,7 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
+from sklearn.ensemble. import RandomForestClassifier
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
@@ -19,8 +20,13 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    #JWC
+
+    model = RandomForestClassifier(n_estimators = 200, max_depth = 50, min_samples_split = 20)
+    model.fit(X_train, y_train)
+    return model
+
+    #JWC
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +65,10 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    #JWC
+    preds = model.predict(X)
+    return preds
+    #JWC
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +80,18 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
-
+    #JWC
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
+    #JWC
+    
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    # JWC
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model    
+    #JWC
 
 
 def performance_on_categorical_slice(
@@ -119,10 +132,15 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        #JWC
+        data[data[column_name] == slice_value],
+        categorical_features = categorical_features,
+        label = label,
+        training = false,
+        encoder = encoder,
+        lb = lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = model.predict(X_slice)
+    #JWC
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
